7bacc2cbc51291c60877d81bf408f033cfa42a18
enmnvv2006 <133098965+enmnvv2006@users.noreply.github.com>
2024-03-02 22:02:44 +0600

    сделал 2 дз

---
+++ geeks-33-01/lesson2/lesson2/src/component/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const List = (props) => {
+    return (
+        <div>
+            {
+                props.newUser.map((item) =>
+                <div style={{ marginTop: '20px' ,border: '3px orange solid', display: 'flex', alignItems: 'center', gap: '10px', paddingLeft: '30px'}}>
+                    <h2>{item.title}</h2>
+                    <p>{item.id}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default List;
